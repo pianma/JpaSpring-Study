bfbb6c903c0d8d5d9b06120e1aa1729b66c9dd72
KimGeonWoo <ikkw2040@naver.com>
2024-01-06 18:11:16 +0900

    commit

---
+++ src/main/java/JpaSpring/JpaSpringstudy/JpaSpringStudyApplication.java
@@ -0,0 +1,13 @@
+package JpaSpring.JpaSpringstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaSpringStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaSpringStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/JpaSpring/JpaSpringstudy/JpaSpringStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package JpaSpring.JpaSpringstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaSpringStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
